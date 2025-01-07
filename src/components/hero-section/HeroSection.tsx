bcc8f2e6e16e8df6aa834e28815ea9801cb005d1
lubnakhan-Github-Account <lk4231455@gmail.com>
2025-01-07 15:27:43 +0500

    somthing try to solve

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -9,61 +9,54 @@ import { FaWhatsapp } from "react-icons/fa";
 import { FaTelegramPlane } from "react-icons/fa";
 import Image from "next/image";
 import { Typewriter } from 'react-simple-typewriter'
-export default function HeroSection(){
+
+export default function HeroSection() {
     return (
         <>
-          <section id="home" className={styles.herosec}>
-               <h2 >Hey ! I m{" "}
-                 <span>U</span>zma <span> K</span>han {" "} 
-
-                 
-             <br /> <span>I </span> m{" "}
-             
+            <section id="home" className={styles.herosec}>
+                <h2>Hey ! I m{" "}
+                    <span>U</span>zma <span> K</span>han {" "}
+                    <br /> <span>I </span> m{" "}
                     <Typewriter
-                 words={[' front end developer','UI UX designner']}
-                 loop={Infinity}         
-                cursor={true} 
-                cursorStyle={'/'}
-                typeSpeed={80} 
-                deleteSpeed={50} 
-                delaySpeed={1500} 
-
-               /> 
-                                    
-                                </h2>
-               <p>I can build user interface for website and applications with  {' '}
-                 <span>React JS</span> and  {' '}
-                 <span>Next JS</span>
-                 <br/> 
-                 I like web development.
-                
-                    </p>
-                 <Button text={"Hire Me"}/>
-                 {/* -------------------------------- */}
-                 <div className={styles.icons}>
-                 <FaFacebookF size={30}  color={"black"}/> 
-                 <FaSquareTwitter size={30} color={"black"}/>
-                 <FaSquareInstagram size={30}  color={"black"}/>
-                 <FaWhatsapp size={30}  color={"black"}/>
-                 <FaTelegramPlane size={30}  color={"black"}/>
-                 </div>
-              {/* -------------------------------------------------------------------------- */}
-                 <div className={styles.bottom}>
+                        words={[' front end developer', 'UI UX designer']}
+                        loop={Infinity}
+                        cursor={true}
+                        cursorStyle={'/'}
+                        typeSpeed={80}
+                        deleteSpeed={50}
+                        delaySpeed={1500}
+                    />
+                </h2>
+                <p>I can build user interface for website and applications with  {' '}
+                    <span>React JS</span> and  {' '}
+                    <span>Next JS</span>
+                    <br />
+                    I like web development.
+                </p>
+                <Button text={"Hire Me"} />
+                {/* -------------------------------- */}
+                <div className={styles.icons}>
+                    <FaFacebookF size={30} color={"black"} />
+                    <FaSquareTwitter size={30} color={"black"} />
+                    <FaSquareInstagram size={30} color={"black"} />
+                    <FaWhatsapp size={30} color={"black"} />
+                    <FaTelegramPlane size={30} color={"black"} />
+                </div>
+                {/* -------------------------------------------------------------------------- */}
+                <div className={styles.bottom}>
                     <Image src={"/images/behance.png"} alt={"pic"} width={156} height={156}></Image>
                     <Image src={"/images/dribble.png"} alt={"pic"} width={156} height={156}></Image>
                     <Image src={"/images/freelance.png"} alt={"pic"} width={156} height={156}></Image>
                     <Image src={"/images/upwork.png"} alt={"pic"} width={156} height={156}></Image>
-                 </div>
+                </div>
             </section>
-
         </>
     )
-
 }
 
 
 
-                 
+
 
 
 
